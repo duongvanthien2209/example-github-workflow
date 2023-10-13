08e48893f35695f0071a6645cc1dbf942d97a156
thien.duongv <thien.duongv@one-line.com>
2023-10-13 19:37:23 +0700

    test case fail workflow

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -9,7 +9,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: /welcome to 11 next\.js!/i,
     })
 
     expect(heading).toBeInTheDocument()
